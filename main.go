d39938c5be6904b7866098f2bdc20177e43e8e2a
DQNEO <dqneoo@gmail.com>
2018-04-16 11:17:22 +0900

    simplify completer

--- main.go
+++ main.go
@@ -29,23 +29,7 @@ func listFiles(path string) func(string) []string {
 }
 
 var completer = readline.NewPrefixCompleter(
-	readline.PcItem("mode",
-		readline.PcItem("vi"),
-		readline.PcItem("emacs"),
-	),
 	readline.PcItem("login"),
-	readline.PcItem("say",
-		readline.PcItemDynamic(listFiles("./"),
-			readline.PcItem("with",
-				readline.PcItem("following"),
-				readline.PcItem("items"),
-			),
-		),
-		readline.PcItem("hello"),
-		readline.PcItem("bye"),
-	),
-	readline.PcItem("setprompt"),
-	readline.PcItem("setpassword"),
 	readline.PcItem("bye"),
 	readline.PcItem("help"),
 	readline.PcItem("go",
